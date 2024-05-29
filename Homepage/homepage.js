9d8c2cba5904c2a627af0efd5c5aa450536aef9c
Khoshang <khoshangkashyap123@gmail.com>
2024-05-29 20:28:54 +0530

    Added go to top button

---
+++ Homepage/homepage.js
@@ -0,0 +1,20 @@
+// Go to top button
+let mybutton = document.getElementById("myBtn");
+
+// changes visibilty after scrolling
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// scrolls to top
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// Go to top button --ends--
